69df7ded8ed0d95f31b65d3947d8636327e387a7
karolina-malolepsza <karolina.malolepsza@schibsted.com>
2023-06-11 21:06:12 +0200

    clear version of 04-api

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,35 +1,18 @@
-import { NoResults } from "./components/NoResults";
-import { PersonCard } from "./components/PersonCard";
-import { Person } from "./types/Person";
-
 export const metadata = {
   title: "Search",
 };
 
 async function getPeople() {
-  const people = await fetch("https://swapi.dev/api/people/");
-
-  return people.json();
+  // Write function to fetch people from the Star Wars API "https://swapi.dev/api/people/"
 }
 
 export default async function Search() {
-  const { results } = await getPeople();
   return (
     <div>
       <h1 className="text-4xl font-semibold text-center mb-20 text-white">
         Star Wars People:
       </h1>
-      {results.length !== 0 ? (
-        <ul className="grid xl:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10 px-4 ">
-          {results.map((person: Person) => (
-            <li key={person.name}>
-              <PersonCard person={person} />
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <NoResults />
-      )}
+      {/*Show result of getPeople() here*/}
     </div>
   );
 }
--- src/app/search/types/Person.ts
+++
@@ -1,6 +0,0 @@
-export type Person = {
-  name: string;
-  height: string;
-  mass: string;
-  hair_color: string;
-};
